3b9b475ee68a580005f3f648bf3360710b46192f
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-05-15 22:06:31 -0500

    I've created the file 'lab_configure_network_rouiting_endpoints.ps1'

---
+++ Scripts/lab_configure_network_rouiting_endpoints.ps1
@@ -0,0 +1,5 @@
+#Interactive lab
+#Author:    Jesus Lopez Mesia
+#Linkedin:  https://www.linkedin.com/in/susejzepol/
+#Date:      May-15-2024
+#Lab:       https://learn.microsoft.com/en-us/training/modules/configure-network-routing-endpoints/7-simulation-routing
