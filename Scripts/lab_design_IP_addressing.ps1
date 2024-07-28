ed09dadfd96c0ae75f0807d62ce8f6fe3050676c
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-07-28 12:29:48 -0500

    Added the command to create the core vnet.

--- Scripts/lab_design_IP_addressing.ps1
+++ Scripts/lab_design_IP_addressing.ps1
@@ -4,3 +4,22 @@
 #Created date:      July-28-2024
 #Modified date:     July-28-2024
 #Lab:               https://learn.microsoft.com/en-us/training/modules/design-ip-addressing-for-azure/5-exercise-implement-vnets
+#Note:              This lab uses a sandbox, the resource group was created by default with the name "learn-95b80900-4065-4559-b217-fda84ce36388".
+
+$rgname                 = "learn-95b80900-4065-4559-b217-fda84ce36388" #JLopez: replace this value with your resource group name.
+$vnetCore               = "CoreServicesVnet"
+$vnetManufactoring      = "ManufacturingVnet"
+$vnetResearch           = "ResearchVnet"
+
+$regionCore             = "West US"
+$regionManufactoring    = "Norht Europe"
+$regionResearch         = "West India"
+
+#Creating the CoreServices Vnet
+az network vnet create `
+    --resource-group $rgname `
+    --name $vnetCore `
+    --address-prefixes 10.20.0.0/16 `
+    --location $regionCore
+
+#Creating the subnets in CoreServices Vnet
