824183afa9b2d1c49fe48770b495d94c7230e234
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-07-28 12:10:32 -0500

    Added the "lab_design_IP_addressing" script to the project.

---
+++ Scripts/lab_design_IP_addressing.ps1
@@ -0,0 +1,6 @@
+#Interactive lab 04
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      July-28-2024
+#Modified date:     July-28-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/design-ip-addressing-for-azure/5-exercise-implement-vnets
