097eca968268e8bd4cc0cfcae42768545b060a3a
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-03 09:34:58 -0500

    I added the file "lab_public_and_private_dns_A_record.ps1" to the project.

---
+++ Scripts/lab_public_and_private_dns_A_record.ps1
@@ -0,0 +1,6 @@
+#Interactive lab
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      August-03-2024
+#Modified date:     August-03-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/host-domain-azure-dns/4-exercise-create-dns-zone-a-record
