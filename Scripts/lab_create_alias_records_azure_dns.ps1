1d33e9f14c6731964517be4c42fe554e3333a43a
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-07 22:14:16 -0500

    added the file "lab_create_alias_records_azure_dns.ps1" ot the project.

---
+++ Scripts/lab_create_alias_records_azure_dns.ps1
@@ -0,0 +1,6 @@
+#Interactive lab 03
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      August-07-2024
+#Modified date:     August-07-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/host-domain-azure-dns/6-exercise-create-alias-records
