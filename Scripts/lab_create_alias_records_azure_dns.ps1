c90ce691e4ba75c730a5065b9bd8f7dbd62a0e11
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-07 22:37:31 -0500

    added some commands to create the virtual network within nsg rule

--- Scripts/lab_create_alias_records_azure_dns.ps1
+++ Scripts/lab_create_alias_records_azure_dns.ps1
@@ -19,3 +19,38 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$subscription
 )
+
+date
+
+#JLopez-20240807: Local variables
+$vnet       = "bePortalVnet"
+$subnet     = "bePortalSubnet"
+$nsg        = "bePortalNSG"
+
+#JLopez-20240807: Creating a virtual network
+Write-Host "Creating a virtual network" -BackgroundColor DarkGreen
+az network vnet create `
+    --resource-group $rg `
+    --location $l `
+    --name $vnet `
+    --subnet-name $subnet
+
+#JLopez-20240807: Creating a network security group
+Write-Host "Creating a network security group" -BackgroundColor DarkGreen
+az network nsg create `
+    --resource-group $rg `
+    --location $l `
+    --name $nsg
+
+az network nsg rule create `
+    --resource-group $rg `
+    --nsg-name $nsg `
+    --name "AllowAll80" `
+    --priority 101 `
+    --source-address-prefixes 'Internet' `
+    --source-port-ranges '*' `
+    --destination-address-prefixes '*'`
+    --destination-port-ranges 80 `
+    --access Allow `
+    --protocol Tcp `
+    --description "Allow all port 80 traffic"
