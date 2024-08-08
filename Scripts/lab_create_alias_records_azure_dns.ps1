5c9eb69c20947448ef062c4206cb4f541a9b7b6b
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-07 22:18:28 -0500

    added some parameters in the script.

--- Scripts/lab_create_alias_records_azure_dns.ps1
+++ Scripts/lab_create_alias_records_azure_dns.ps1
@@ -4,3 +4,18 @@
 #Created date:      August-07-2024
 #Modified date:     August-07-2024
 #Lab:               https://learn.microsoft.com/en-us/training/modules/host-domain-azure-dns/6-exercise-create-alias-records
+
+[CmdletBinding()]
+param (
+    #JLopez-20240807: Location
+    [Parameter(Mandatory=$true)]
+    [string]$l,
+
+    #JLopez-20240807: Resource group name
+    [Parameter(Mandatory=$true)]
+    [string]$rg,
+
+    #JLopez-20240807: Subscription name
+    [Parameter(Mandatory=$true)]
+    [string]$subscription
+)
