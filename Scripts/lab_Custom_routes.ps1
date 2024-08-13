706611670dda41640d83ddf3f21f993f87858368
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-12 23:07:45 -0500

    added to the project.

---
+++ Scripts/lab_Custom_routes.ps1
@@ -0,0 +1,6 @@
+#Interactive lab
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      August-12-2024
+#Modified date:     August-12-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/control-network-traffic-flow-with-routes/3-exercise-create-custom-routes
