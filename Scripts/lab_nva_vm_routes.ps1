061835a07a1eddc8b050bb8c29c7add388c38eac
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-21 17:03:35 -0500

    Added the file to the project.

---
+++ Scripts/lab_nva_vm_routes.ps1
@@ -0,0 +1,6 @@
+#Interactive lab 
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      August-21-2024
+#Modified date:     August-21-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/control-network-traffic-flow-with-routes/5-exercise-create-nva-vm
