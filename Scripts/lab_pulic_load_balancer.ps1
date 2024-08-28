9c5de340c775cf0faa070726c0d7b9bf50c647e1
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-08-28 16:16:00 -0500

    Added the file to the project

---
+++ Scripts/lab_pulic_load_balancer.ps1
@@ -0,0 +1,6 @@
+#Interactive lab 
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      August-28-2024
+#Modified date:     August-28-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/improve-app-scalability-resiliency-with-load-balancer/4-exercise-configure-public-load-balancer?pivots=bash
