8e25fbcda244e66d5815019be188f39da83024d3
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-09-06 17:59:39 -0500

    Added the "printMyMessage" function to the script.

--- Scripts/utilities/print-message-custom-v1.psm1
+++ Scripts/utilities/print-message-custom-v1.psm1
@@ -2,3 +2,22 @@
 #Linkedin:          https://www.linkedin.com/in/susejzepol/
 #Created date:      September-06-2024
 #Modified date:     September-06-2024
+
+#JLopez-20240906: Funtion to print a custom message in console.
+function printMyMessage {
+    param (
+        [string]$message,
+        [string]$color ="DarkGreen"
+    )
+    
+    Write-Host "______________________________________________________" -BackgroundColor $color
+    Write-Host "$message" -BackgroundColor DarkGreen
+    Write-Host "______________________________________________________" -BackgroundColor $color
+    
+    for ($i = 0; $i -lt 6; $i++) {
+        Write-Host ""
+    }
+}
+
+#JLopez-20240906: Exporting functions to make them available when the module is imported.
+Export-ModuleMember -Function printMyMessage
