b1a6e4d679f865d3e0c04ef7b67969b76f62f3ed
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-09-17 15:21:28 -0500

    Added 1 file to the project under scripts folder

---
+++ Scripts/00012_storage_account.ps1
@@ -0,0 +1,5 @@
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      September-17-2024
+#Modified date:     September-17-2024
+#Lab:               https://learn.microsoft.com/en-us/training/modules/configure-storage-security/8-simulation-storage
