f037c4ef2f3cc69e95769810f17675da2e2cb2f1
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-09-18 10:36:43 -0500

    I added to the script the function "" which is responsible to validate if a resource group exists in a specific subscription.

--- Scripts/utilities/print-message-custom-v1.psm1
+++ Scripts/utilities/print-message-custom-v1.psm1
@@ -12,13 +12,45 @@ function printMyMessage {
     )
     
     Write-Host "______________________________________________________" -BackgroundColor $color
-    Write-Host "$message" -BackgroundColor DarkGreen
+    Write-Host "$message" -BackgroundColor $color
     Write-Host "______________________________________________________" -BackgroundColor $color
     
     for ($i = 0; $i -lt $c; $i++) {
         Write-Host ""
     }
 }
+#JLopez-20240918:   This function should validate if the resources group exist in the current subscription. 
+#                   If it does not exist I will create it.
+function checkMyResourceGroup {
+    param(
+        #Resource group
+        [Parameter(Mandatory=$true)]
+        [string]$rg,
+        #Subscription
+        [Parameter(Mandatory=$true)]
+        [string]$s,
+        #Location
+        [Parameter(Mandatory=$true)]
+        [string]$l,
+        #Tags
+        [string]$t,
+        #BackgroundColor
+        [string]$bc = "DarkGreen"
+    )
+
+    $check_rg = -not [bool]::Parse($(az group exists --name $rg))
+    #JLopez: If the resource group not exists.
+    if ($check_rg) {
+        Write-Host "The resource group ($rg) doesn't exists in the current subscription ($s)." -BackgroundColor $bc
+        #JLopez: Then create the resource group.
+        Write-Host "Creating the resource group ($rg) in the location ($l)." -BackgroundColor $bc
+        az group create `
+            --name $rg `
+            --subscription $s `
+            --location $l `
+            --tags $t
+    }
+}
 
 #JLopez-20240906: Exporting functions to make them available when the module is imported.
-Export-ModuleMember -Function printMyMessage
+Export-ModuleMember -Function printMyMessage,checkMyResourceGroup
