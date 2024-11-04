d5ffb99435b4d5ad8d4a5baed3bd60981e414ece
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-11-03 19:48:45 -0500

    I fixed the command to add the alert processing rule to add more action groups to all alerts.

--- Scripts/00013_Azure_Monitor_Activity_Alert.ps1
+++ Scripts/00013_Azure_Monitor_Activity_Alert.ps1
@@ -1,7 +1,7 @@
 #Author:            Jesus Lopez Mesia
 #Linkedin:          https://www.linkedin.com/in/susejzepol/
 #Created date:      October-23-2024
-#Modified date:     November-02-2024
+#Modified date:     November-03-2024
 #Lab:               https://learn.microsoft.com/en-us/training/modules/incident-response-with-alerting-on-azure/8-exercise-activity-log-alerts
 
 [CmdletBinding()]
@@ -26,7 +26,7 @@ Write-Host "$(get-date)" -BackgroundColor DarkGreen
 
 #JLopez: Internal variables
 $day                    = $(get-date -format "yyyyMMdd")
-$lab                    = "lab0003" + $day
+$lab                    = "lab000131" + $day
 $rg1                    = $lab + "az104"
 $vnet                   = $lab + "Vnet"
 $subnet                 = $lab + "Subnet"
@@ -37,6 +37,7 @@ $action_group1          = $lab + "1ag"
 $action_group2          = $lab + "2ag"
 $activity_log_alert     = "VMDeletionAlert"
 $metric_alert           = "Cpu80PercentAlert"
+$analytics_workspace    = "defaultaz104"
 
 printMyMessage -message "Starting with the resource group validation." -c 0
 checkMyResourceGroup -rg $rg1 -s $s -l $l -t Project=$lab
@@ -47,6 +48,7 @@ az configure --defaults group=$rg1
 
 $vm1_name = $vm + "1"
 $vm2_name = $vm + "2"
+$vm3_name = $vm + "3"
 
 #JLopez: Here I used the "2>$null" to discard any error message produced by the command. 
 az vm show --name $vm1_name --output none 2>$null
@@ -85,9 +87,13 @@ if($LASTEXITCODE -ne 0){
 
     printMyMessage -message "Network security group deployed!."
 
+    az monitor log-analytics workspace create `
+        --workspace-name $analytics_workspace `
+        --location $l
+
     printMyMessage -message "Starting the virtual machines creation." -c 0
 
-    for ($i = 1; $i -lt 3; $i++) {
+    for ($i = 1; $i -le 3; $i++) {
 
         $public_ip  = "publicIP" + $i
         $pvm         = $vm + $i
@@ -118,17 +124,13 @@ if($LASTEXITCODE -ne 0){
         --no-wait `
         --tags Project=$lab
 
-        az monitor log-analytics workspace create `
-            --workspace-name "defaultaz104" `
-            --location $l
-
         $vm_id = $(az vm show --name $pvm --query "id" --output tsv)
         $name_diagnosis_log ="checkMy_" + $pvm
         Write-Host -message "Enabling monitor diagnosis for each virtual machine!." -BackgroundColor DarkGreen
         az monitor diagnostic-settings create `
             --name $name_diagnosis_log `
             --resource $vm_id `
-            --workspace "defaultaz104"
+            --workspace $analytics_workspace 
     }
     
 }else{
@@ -166,6 +168,7 @@ do {
 
 $vm1ID = $(az vm show --name $vm1_name --query "id"--output tsv)
 $vm2ID = $(az vm show --name $vm2_name --query "id"--output tsv)
+$vm3ID = $(az vm show --name $vm3_name --query "id"--output tsv)
 
 Write-Host "Checking if the alert ($metric_alert) exists."  -BackgroundColor DarkGreen
 az monitor metrics alert show --name $metric_alert --output none 2>$null
@@ -176,7 +179,7 @@ if($LASTEXITCODE -ne 0){
     az monitor metrics alert create `
         --name $metric_alert `
         --resource-group $rg1 `
-        --scopes $vm1ID $vm2ID `
+        --scopes $vm1ID $vm2ID $vm3ID `
         --condition "max percentage CPU > 80" `
         --description "Virtual machine is running at or greater than 80% CPU utilization" `
         --evaluation-frequency 1m `
@@ -197,7 +200,7 @@ if($LASTEXITCODE -ne 0){
     Write-Host "Creating a new activity log alert for each virtual machine in case of deletion." -BackgroundColor DarkGreen
     az monitor activity-log alert create `
         --name $activity_log_alert `
-        --scope $vm1ID $vm2ID `
+        --scope $vm1ID $vm2ID $vm3ID `
         --condition category=Administrative and operationName='Microsoft.Compute/virtualMachines/delete' `
         --action-group $action_group1 `
         --description "The virtual machine was deleted." `
@@ -206,7 +209,6 @@ if($LASTEXITCODE -ne 0){
 }else{
     Write-Host "The activity log alert already exists. No further action is needed." -BackgroundColor DarkGreen
 }
-
 
 Write-host "Deleting the virtual machine ($vm1_name) to trigger the <VMDeletionAlert>." -BackgroundColor DarkYellow
 az vm delete `
@@ -218,19 +220,9 @@ Write-host "The virtual machine ($vm1_name) was deleted." -BackgroundColor DarkG
 
 $ResourcegroupId = $(az group show --name $rg1 --query "id" --output tsv)
 
-# Write-Host "Adding a alert processing rule to remove all notifications from previous rules." -BackgroundColor DarkGreen
-# az monitor alert-processing-rule create `
-#     --name "Remove notifications due to maintenance window" `
-#     --rule-type RemoveAllActionGroups `
-#     --scopes $ResourcegroupId `
-#     --filter-resource-type Equals "microsoft.compute/virtualmachines" `
-#     --description "Removes all notifications from action groups from all alerts."
-
-# Write-Host "Processing rule added." -BackgroundColor DarkGreen
-
 Write-Host "Adding a alert processing rule to add an action group to revious rules." -BackgroundColor DarkGreen
 
-$action_group2_id = $( az monitor action-group show $action_group2 --query "id" --output tsv)
+$action_group2_id = $( az monitor action-group show --name $action_group2 --query "id" --output tsv)
 
 az monitor alert-processing-rule create `
     --name "add notification group" `
@@ -247,4 +239,15 @@ az vm delete `
     --force-deletion true `
     --yes
 Write-host "The virtual machine ($vm2_name) was deleted." -BackgroundColor DarkGreen
+
+# Write-Host "Adding a alert processing rule to remove all notifications from previous rules." -BackgroundColor DarkGreen
+# az monitor alert-processing-rule create `
+#     --name "Remove notifications due to maintenance window" `
+#     --rule-type RemoveAllActionGroups `
+#     --scopes $ResourcegroupId `
+#     --filter-resource-type Equals "microsoft.compute/virtualmachines" `
+#     --description "Removes all notifications from action groups from all alerts."
+
+# Write-Host "Processing rule added." -BackgroundColor DarkGreen
+
 printMyMessage -message "All Alerts Deployed!." -c 0
