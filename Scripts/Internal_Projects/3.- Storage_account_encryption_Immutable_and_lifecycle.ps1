ca30e17a55d11bcbc3854f9c2be100e7c1879eb8
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-11-21 11:02:30 -0500

    added a block code to create a storage account 1 with the inmmutability option enable.

--- Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
+++ Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
@@ -1,7 +1,7 @@
 #Author         :   Jesus Lopez Mesia
 #Linkedin       :   https://www.linkedin.com/in/susejzepol/
 #Created date   :   November-20-2024
-#Modified date  :   November-20-2024
+#Modified date  :   November-21-2024
 #script Purpose :   Manage storage account lifecycle rules, Encryption and immutable storage polices.
 
 #JLopez: Import the module "print-message-custom-v1.psm1".
@@ -19,15 +19,33 @@ Write-Host "$(get-date)" -BackgroundColor DarkGreen
 #JLopez: Internal variables
 $date                   = $(get-date -format "MMdd")
 $project                = "IP3_1_" + $date
-$location               = "South Central US"
+$location1              = "South Central US"
+$location2              = "East US"
 $rg                     = "rg_" + $project 
-$storage_account        = "sa_" + $project
+$vnet                   = "vnet_" + $Project
+
+#JLopez-20241121:   The storage account name must be unique across all existing storage account names in Azure. 
+#                   It must be 3 to 24 characters long, and can contain only lowercase letters and numbers.
+$storage_account1       = "sa1" + $project.Replace("_","").ToLower()
+$storage_account2       = "sa2" + $project.Replace("_","").ToLower()
+$storage_account3       = "sadata" + $project.Replace("_","").ToLower()
+$storage_account4       = "sareplica_" + $project.Replace("_","").ToLower()
 
 printMyMessage -message "Starting with the resource group validation." -c 0
 
-checkMyResourceGroup -rg $rg -l $location -t Project=$project
+checkMyResourceGroup -rg $rg -l $location1 -t Project=$project
 
 Write-Host "Setting the default resource group to $rg." -BackgroundColor DarkGreen
 az configure --defaults group=$rg
 
 printMyMessage -message "Resource group validation done!."
+
+printMyMessage -message "Deploying the ($storage_account1) storage account." -c 0
+
+az storage account create `
+    --name $storage_account1 `
+    --sku 'Standard_LRS' `
+    --location $location1 `
+    --enable-alw true #Jlopez: this option enable both versioning and Version-Level Immutability Support option for the storage account.
+
+printMyMessage -message "($storage_account1) was deployed!."
