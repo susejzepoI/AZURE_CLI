92d9a284036dc2512450bec101355584a82ae98b
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-11-26 23:29:05 -0500

    added a virtual network, enable service endpoint and added the vnet to the storage account1.

--- Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
+++ Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
@@ -1,7 +1,7 @@
 #Author         :   Jesus Lopez Mesia
 #Linkedin       :   https://www.linkedin.com/in/susejzepol/
 #Created date   :   November-20-2024
-#Modified date  :   November-25-2024
+#Modified date  :   November-26-2024
 #Script Purpose :   Manage storage account lifecycle rules, Encryption, Immutable blob storage and Stored access polices.
 
 #JLopez: Import the module "print-message-custom-v1.psm1".
@@ -18,18 +18,18 @@ Write-Host "$(get-date)" -BackgroundColor DarkGreen
 
 #JLopez: Internal variables
 $date                   = $(get-date -format "MMdd")
-$project                = "IP3_2_" + $date
+$project                = "IP3_1_"      + $date
 $location1              = "South Central US"
 $location2              = "East US"
-$rg                     = "rg_" + $project 
-$vnet                   = "vnet_" + $Project
-
+$rg                     = "rg_"         + $project 
+$vnet                   = "vnet_"       + $Project
+$subnet                 = "subnet1_"    + $project
 #JLopez-20241121:   The storage account name must be unique across all existing storage account names in Azure. 
 #                   It must be 3 to 24 characters long, and can contain only lowercase letters and numbers.
-$storage_account1       = "sa1" + $project.Replace("_","").ToLower()
-$storage_account2       = "sa2" + $project.Replace("_","").ToLower()
-$storage_account3       = "sadata" + $project.Replace("_","").ToLower()
-$storage_account4       = "sareplica_" + $project.Replace("_","").ToLower()
+$storage_account1       = "sa1"         + $project.Replace("_","").ToLower()
+$storage_account2       = "sa2"         + $project.Replace("_","").ToLower()
+$storage_account3       = "sadata"      + $project.Replace("_","").ToLower()
+$storage_account4       = "sareplica_"  + $project.Replace("_","").ToLower()
 
 printMyMessage -message "Starting with the resource group validation." -c 0
 
@@ -39,6 +39,25 @@ Write-Host "Setting the default resource group to $rg." -BackgroundColor DarkGre
 az configure --defaults group=$rg
 
 printMyMessage -message "Resource group validation done!."
+
+printMyMessage -message "Deploying the ($storage_account1) virtual network." -c 0
+
+az network vnet create `
+    --name $vnet `
+    --subnet-name $subnet `
+    --location $location1 `
+    --tags Project=$project
+
+Write-Host "Enabling service endpoint for the subnet1." -BackgroundColor DarkGreen
+#JLopez-20241126: This must be enabled in order to add the virtual network to the storage account.
+az network vnet subnet update `
+    --vnet-name $vnet `
+    --name $subnet `
+    --service-endpoints Microsoft.Storage
+
+Write-Host "(Microsoft.Storage) service endpoint was added in the subnet1." -BackgroundColor DarkGreen
+
+printMyMessage -message "Virtual network deplyed!."
 
 printMyMessage -message "Deploying the ($storage_account1) storage account." -c 0
 
@@ -51,6 +70,9 @@ if ($LASTEXITCODE -ne 0 ) {
         --name $storage_account1 `
         --sku 'Standard_LRS' `
         --location $location1 `
+        --tags Project=$Project `
+        --vnet-name $vnet `
+        --subnet $subnet `
         --enable-alw true #Jlopez: this option enable both versioning and Version-Level Immutability Support option for the storage account.
 
     Write-Host "Creating the (backups) container." -BackgroundColor DarkGreen
