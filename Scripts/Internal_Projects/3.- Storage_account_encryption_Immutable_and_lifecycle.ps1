7df920c269af717b8bad28f5bc0ad1d130dd47c5
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2024-12-03 23:04:10 -0500

    added one azure key vault and a RSA key. Added a container call images in the second storage account.

--- Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
+++ Scripts/Internal_Projects/3.- Storage_account_encryption_Immutable_and_lifecycle.ps1
@@ -1,7 +1,7 @@
 #Author         :   Jesus Lopez Mesia
 #Linkedin       :   https://www.linkedin.com/in/susejzepol/
 #Created date   :   November-20-2024
-#Modified date  :   December-02-2024
+#Modified date  :   December-03-2024
 #Script Purpose :   Manage storage account lifecycle rules, Encryption, Immutable blob storage and Stored access polices.
 
 #JLopez: Import the module "print-message-custom-v1.psm1".
@@ -18,19 +18,20 @@ Write-Host "$(get-date)" -BackgroundColor DarkGreen
 
 #JLopez: Internal variables
 $date                   = $(get-date -format "MMdd")
-$project                = "IP3_1_"      + $date
+$project                = "IP3_2_"      + $date
 $location1              = "South Central US"
 $location2              = "East US"
 $rg                     = "rg_"         + $project 
 $vnet                   = "vnet_"       + $Project
 $subnet                 = "subnet1_"    + $project
-$key_vault              = "vault_"      + $project
-#JLopez-20241121:   The storage account name must be unique across all existing storage account names in Azure. 
-#                   It must be 3 to 24 characters long, and can contain only lowercase letters and numbers.
+#JLopez-20241121: The storage account name must be unique across all existing storage account names in Azure. 
+#JLopez-20241203: The storage account and vault name must be 3 to 24 characters long, and can contain only lowercase letters and numbers.
+$key_vault              = "vault"      + $project.Replace("_","").ToLower()
+$key1                   = "key"         + $project.Replace("_","").ToLower()
 $storage_account1       = "sa1"         + $project.Replace("_","").ToLower()
 $storage_account2       = "sa2"         + $project.Replace("_","").ToLower()
 $storage_account3       = "sadata"      + $project.Replace("_","").ToLower()
-$storage_account4       = "sareplica_"  + $project.Replace("_","").ToLower()
+$storage_account4       = "sareplica"   + $project.Replace("_","").ToLower()
 
 printMyMessage -message "Starting with the resource group validation." -c 0
 
@@ -159,12 +160,29 @@ if ($LASTEXITCODE -ne 0 ) {
         --name $storage_account2 `
         --sku 'Standard_LRS' `
         --location $location2 `
-        --tags Project=$Project 
+        --tags Project=$Project
 
+    Write-Host "Creating the (images) container." -BackgroundColor DarkGreen
+    az storage container create `
+        --name "images" `
+        --account-name $storage_account2
+
+    Write-Host "Creating the key vault ($key_vault)." -BackgroundColor DarkGreen
     az keyvault create `
         --name $key_vault `
-        --location $location2
+        --location $location2 `
+        --tags Project=$project `
+        --enable-rbac-authorization false ` #JLopez: to force vault access policies.
 
+    Write-Host "Creating a new key ($key1) in the ($key_vault) vault." -BackgroundColor DarkGreen
+    az keyvault key create `
+        --vault-name $key_vault `
+        --name $key1 `
+        --protection software `
+        --kty RSA `
+        --size 4096 `
+
+    
 }else{
     Write-Host "The storage account ($storage_account2) already exists, no further action is required." -BackgroundColor DarkYellow
 }
