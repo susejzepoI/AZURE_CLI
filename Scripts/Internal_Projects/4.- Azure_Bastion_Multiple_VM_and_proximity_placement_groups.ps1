7dd1dac45ad047e784681db79901d7fb08f03a92
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-01-03 17:35:32 -0500

    renamed the variables to refer to the hub and spoke topology. Added the virtual network peering between the hub network and the spoke networks. Remove the location 2,3 and 4 (because the azure bastion only supports vnets in the same region).

--- Scripts/Internal_Projects/4.- Azure_Bastion_Multiple_VM_and_proximity_placement_groups.ps1
+++ Scripts/Internal_Projects/4.- Azure_Bastion_Multiple_VM_and_proximity_placement_groups.ps1
@@ -19,17 +19,14 @@ Import-Module  "$root\utilities\print-message-custom-v1.psm1"
 Write-Host "$(get-date)" -BackgroundColor DarkGreen
 
 #JLopez: Internal variables
-$date                       = $(get-date -format "MMdd")
+$date                       = $(get-date -format "yyyyMMdd")
 $project                    = "IP4_1_"      + $date
 $location1                  = "South Central US"
-$location2                  = "East US"
-$location3                  = "West US"
-$location4                  = "West US 2"
-$public_ip                  = "bastion-ip"       
-$rg1                        = "rg1_"        + $project 
-$rg2                        = "rg2_"        + $project
-$rg3                        = "rg3_"        + $project 
-$rg4                        = "rg4_"        + $project 
+$public_ip                  = "Hubbastion-ip"       
+$rg1                        = "rg1_spoke_"        + $project 
+$rg2                        = "rg2_spoke_"        + $project
+$rg3                        = "rg3_hub_"        + $project 
+$rg4                        = "rg4_spoke_"        + $project 
 $vm                         = "vm"
 $nic                        = "nic"
 $vnet                       = "vnet"
@@ -37,16 +34,16 @@ $vnet                       = "vnet"
 printMyMessage -message "Starting with the resource groups validations." -c 0
 
     checkMyResourceGroup -rg $rg1 -l $location1 -t Project=$project
-    checkMyResourceGroup -rg $rg2 -l $location2 -t Project=$project
-    checkMyResourceGroup -rg $rg3 -l $location3 -t Project=$project
-    checkMyResourceGroup -rg $rg4 -l $location4 -t Project=$project
+    checkMyResourceGroup -rg $rg2 -l $location1 -t Project=$project
+    checkMyResourceGroup -rg $rg3 -l $location1 -t Project=$project
+    checkMyResourceGroup -rg $rg4 -l $location1 -t Project=$project
 
 printMyMessage -message "Resource groups validations done!."
 
 
 printMyMessage -message "Starting with the virtual machine deployment." -c 0
 
-    for ($i = 3; $i -le 3; $i++) {
+    for ($i = 1; $i -le 4; $i++) {
 
         $vnet_name = $vnet.Replace("vnet", "vnet$i")
         $nic_name = $nic.Replace("nic", "nic$i")
@@ -55,7 +52,6 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
         switch ($i) {
             1 
                 { 
-                    $l = $location1
                     $rg = $rg1
                     $vnet_address               = "11.0.0.0/16"
                     $default_subnet             = "default" 
@@ -66,7 +62,6 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
                 }
             2 
                 { 
-                    $l = $location2
                     $rg = $rg2
                     $vnet_address               = "12.0.0.0/16"
                     $default_subnet             = "default" 
@@ -77,7 +72,6 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
                 }
             3 
                 {   
-                    $l = $location3
                     $rg = $rg3
                     $vnet_address               = "13.0.0.0/16"
                     $default_subnet             = "default" 
@@ -88,7 +82,6 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
                 }
             default 
                 {   
-                    $l = $location4
                     $rg = $rg4
                     $vnet_address               = "14.0.0.0/16"
                     $default_subnet             = "default" 
@@ -107,7 +100,7 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
             --name $vnet_name `
             --address-prefixes $vnet_address `
             --subnets "[{'name':'$default_subnet','addressPrefix':'$default_subnet_address'},{'name':'$bastion_subnet','addressPrefix':'$bastion_subnet_address'}]" `
-            --location $l `
+            --location $location1 `
             --tags Project=$project
 
         Write-Host "Deploying the $nic_name in $rg." -BackgroundColor DarkGreen
@@ -115,7 +108,7 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
             --name $nic_name `
             --vnet-name $vnet_name `
             --subnet $default_subnet `
-            --location $l `
+            --location $location1 `
             --tags Project=$project
 
         Write-Host "Deploying the $vm_name in $rg." -BackgroundColor DarkGreen
@@ -126,23 +119,76 @@ printMyMessage -message "Starting with the virtual machine deployment." -c 0
             --admin-username azureuser `
             --admin-password "3000@UserAzure" `
             --nics $nic_name `
-            --location $l `
+            --location $location1 `
             --tags Project=$project `
             --no-wait
     }
 
 printMyMessage -message "All virtual machines were deployed!."
 
-Write-Host "Setting the default resource group to $rg3." -BackgroundColor DarkGreen
+printMyMessage -message "Starting peering the hub network with the spoke networks." -c 0
+    $vnet1_id     = $(az network vnet show --name "vnet1" --resource-group $rg1 --query "id" --output tsv)
+    $vnet2_id     = $(az network vnet show --name "vnet2" --resource-group $rg2 --query "id" --output tsv)
+    $vnet3_id     = $(az network vnet show --name "vnet3" --resource-group $rg3 --query "id" --output tsv)
+    $vnet4_id     = $(az network vnet show --name "vnet4" --resource-group $rg4 --query "id" --output tsv)
+
+    Write-Host "Peering vnet3 (hub) with vnet1 (spoke)." -BackgroundColor DarkGreen
+    az network vnet peering create `
+        --name "vnet3-to-vnet1" `
+        --resource-group $rg3 `
+        --vnet-name "vnet3" `
+        --remote-vnet $vnet1_id `
+        --allow-vnet-access true
+
+    az network vnet peering create `
+        --name "vnet1-to-vnet3" `
+        --resource-group $rg1 `
+        --vnet-name "vnet1" `
+        --remote-vnet $vnet3_id `
+        --allow-vnet-access true
+
+    Write-Host "Peering vnet3 (hub) with vnet2 (spoke)." -BackgroundColor DarkGreen
+    az network vnet peering create `
+        --name "vnet3-to-vnet2" `
+        --resource-group $rg3 `
+        --vnet-name "vnet3" `
+        --remote-vnet $vnet2_id `
+        --allow-vnet-access true
+
+    az network vnet peering create `
+        --name "vnet2-to-vnet3" `
+        --resource-group $rg2 `
+        --vnet-name "vnet2" `
+        --remote-vnet $vnet3_id `
+        --allow-vnet-access true
+
+    Write-Host "Peering vnet3 (hub) with vnet4 (spoke)." -BackgroundColor DarkGreen
+    az network vnet peering create `
+        --name "vnet3-to-vnet4" `
+        --resource-group $rg3 `
+        --vnet-name "vnet3" `
+        --remote-vnet $vnet4_id `
+        --allow-vnet-access true
+
+    az network vnet peering create `
+        --name "vnet4-to-vnet3" `
+        --resource-group $rg4 `
+        --vnet-name "vnet4" `
+        --remote-vnet $vnet3_id `
+        --allow-vnet-access true
+
+printMyMessage -message "All networks peered."
+
+Write-Host "Setting the default resource group to $rg3 (hub resource group)." -BackgroundColor DarkGreen
 az configure --defaults group=$rg3
 
-printMyMessage -message "Deploying the Bastion instance in ($location3) location." -c 0
+printMyMessage -message "Deploying the Bastion instance in ($location1) location." -c 0
 
     Write-Host "Deploying the public ip address for the bastion." -BackgroundColor DarkGreen
     #JLopez-20250103: The public ip need to be in the same region where the bastion is going to be deployed.
     az network public-ip create `
         --name $public_ip `
-        --location $location3 `
+        --location $location1 `
         --sku "Standard" `
         --allocation-method "Static" `
         --tags Project=$project
