26de50e7194cde52f491d62f64e02645c587306b
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-01-08 12:42:54 -0500

    added "0.- Clean_all_resources_by_tags.ps1" to the internal project folder.

---
+++ Scripts/Internal_Projects/0.- Clean_all_resources_by_tags.ps1
@@ -0,0 +1,6 @@
+#Author         :   Jesus Lopez Mesia
+#Linkedin       :   https://www.linkedin.com/in/susejzepol/
+#Created date   :   January-08-2025
+#Modified date  :   January-08-2025
+#Script Purpose :   This script delete all the resources deployed in resources
+#                   groups with an specif tag.
