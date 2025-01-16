c9674d7b11a28fea98deec6a4920c505591bc806
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-01-16 17:03:23 -0500

    created the file "5.- Azure_Storage_Account_Static_Web.ps1". Added a new storage account and enable the static web feature.

---
+++ Scripts/Internal_Projects/5.- Azure_Storage_Account_Static_Web.ps1
@@ -0,0 +1,61 @@
+#Author         :   Jesus Lopez Mesia
+#Linkedin       :   https://www.linkedin.com/in/susejzepol/
+#Created date   :   January-16-2025
+#Modified date  :   January-16-2025
+#Script Purpose :   This script deploys a storage account with a static web page.
+
+#JLopez: Import the module "print-message-custom-v1.psm1".
+if($pwd.path -like "*Scripts"){
+    $root = "."
+}else {
+    $root = ".\Scripts"
+}
+Import-Module  "$root\utilities\print-message-custom-v1.psm1"
+
+Write-Host "$(get-date)" -BackgroundColor DarkGreen
+
+#JLopez: Internal variables
+$date                       = $(get-date -format "yyyyMMdd")
+$project                    = "IP5_3_"      + $date
+$location                   = "West US"
+$rg                         = "rg_"         + $project 
+$storage_account            = "st"          + $project.Replace("_","").ToLower()
+
+
+printMyMessage -message "Starting with the resource groups validations." -c 0
+
+    checkMyResourceGroup -rg $rg -l $location -t Project=$project
+
+printMyMessage -message "Resource groups validations done!."
+
+printMyMessage -message "Creating a new storage account ($storage_account)." -c 0
+
+    Write-Host "Setting the default resource group to $rg." -BackgroundColor DarkGreen
+    az configure --defaults group=$rg
+    #JLopez-20250116: In order to create a static web page in the storage account,
+    #                 you need to create a general purpose v2 or BlobStorage storage account.
+    #                 Also, you need to enable the static website feature.
+    az storage account create `
+        --name $storage_account `
+        --location $location `
+        --sku Standard_LRS `
+        --kind StorageV2 `
+        --access-tier Hot `
+        --tags Project=$project `
+        --allow-blob-public-access true
+    
+    Write-Host "Enabling the static website feature." -BackgroundColor DarkGreen
+    $primaryEndpoint = 
+                        $(
+                            az storage blob service-properties update `
+                                --account-name $storage_account `
+                                --static-website `
+                                --index-document index.html `
+                                --404-document error.html `
+                                --query "primaryEndpoints.web" `
+                                --output tsv
+                        )
+    Write-Host "The endpoint for the web site app is: $primaryEndpoint" -BackgroundColor DarkYellow
+
+
+printMyMessage -message "The storage account ($storage_account) was deployed."
