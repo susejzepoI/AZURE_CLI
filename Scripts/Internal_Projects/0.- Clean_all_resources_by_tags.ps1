25a8bd56c7c4dd8a1fe004efdf0679657a677589
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-01-16 15:51:38 -0500

    merge main branch with the internal project branch

--- Scripts/Internal_Projects/0.- Clean_all_resources_by_tags.ps1
+++ Scripts/Internal_Projects/0.- Clean_all_resources_by_tags.ps1
@@ -2,7 +2,7 @@
 #Linkedin       :   https://www.linkedin.com/in/susejzepol/
 #Created date   :   January-08-2025
 #Modified date  :   January-16-2025
-#Script Purpose :   This script delete all the resources deployed in resources
+#Script Purpose :   This script deletes all the resources deployed in resources
 #                   groups with an specif tag.
 
 [CmdletBinding()]
