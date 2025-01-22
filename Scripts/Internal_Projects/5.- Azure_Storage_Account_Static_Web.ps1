43f3a0cbf5cbdc6995c9fc469c1c55f5782b0440
Jesús Miguel Lopez mesia <jesus_lopez_mm1995@outlook.com>
2025-01-21 23:12:46 -0500

    I add a html and css file into the "Internal_Projects\Utilities" folder and added a command to upload these files into my azure storage account (witout testing).

--- Scripts/Internal_Projects/5.- Azure_Storage_Account_Static_Web.ps1
+++ Scripts/Internal_Projects/5.- Azure_Storage_Account_Static_Web.ps1
@@ -1,7 +1,7 @@
 #Author         :   Jesus Lopez Mesia
 #Linkedin       :   https://www.linkedin.com/in/susejzepol/
 #Created date   :   January-16-2025
-#Modified date  :   January-20-2025
+#Modified date  :   January-21-2025
 #Script Purpose :   This script deploys a storage account with a static web page.
 
 #JLopez: Import the module "print-message-custom-v1.psm1".
@@ -16,7 +16,7 @@ Write-Host "$(get-date)" -BackgroundColor DarkGreen
 
 #JLopez: Internal variables
 $date                       = $(get-date -format "yyyyMMdd")
-$project                    = "IP5_2_"      + $date
+$project                    = "IP5_1_"      + $date
 $location                   = "West US"
 $rg                         = "rg_"         + $project 
 $storage_account            = "st"          + $project.Replace("_","").ToLower()
@@ -58,7 +58,15 @@ printMyMessage -message "Creating a new storage account ($storage_account)." -c 
                                 --output tsv
                         )
 
-    Write-Host "The endpoint for the web site app is: $primaryEndpoint" -BackgroundColor DarkYellow
-
-
 printMyMessage -message "The storage account ($storage_account) was deployed."
+
+printMyMessage -message "Uploading the webside files into the $web container." -c 0
+    
+    az storage blob upload-batch `
+        --account-name $storage_account `
+        --destination "$web" `
+        --source ".\Utilities\" `
+        --pattern "*.html"	
+
+printMyMessage -message "Webside files deployed into the $web container."
+Write-Host "The endpoint for the web site app is: $primaryEndpoint" -BackgroundColor DarkYellow
